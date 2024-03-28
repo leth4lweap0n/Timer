f35ef0c3814506727431b76ed4341b2c5029c844
Leth4Lweap0N <leth4lweap0n@outlook.com>
2024-03-29 01:09:55 +0300

    Create Entry.cpp

---
+++ Entry.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <chrono>
+#include <functional>
+#include <future>
+#include <thread>
+
+template<typename Interval>
+class Timer {
+public:
+	Timer() : expired_(true) {}
+
+	void Start(int interval, const _STD function<void()>& func) {
+		expired_ = false;
+		interval_ = Interval(interval);
+		future_ = _STD async(_STD launch::async, [this, func]() {
+			while (!expired_) {
+				_STD this_thread::sleep_for(interval_);
+				func();
+			}
+			});
+	}
+
+	void Stop() {
+		expired_ = true;
+		future_.wait();
+	}
+
+	bool IsRunning() const
+	{
+		return !expired_;
+	}
+
+private:
+	_STD future<void> future_;
+	Interval interval_;
+	bool expired_;
+};
+
+
+void TimerMicrosecondsDoEvent()
+{
+	_STD cout << "TimerMicrosecondsDoEvent..." << '\n';
+}
+
+void TimerMillisecondsDoEvent()
+{
+	_STD cout << "TimerMillisecondsDoEvent..." << '\n';
+}
+
+void TimerSecondsDoEvent()
+{
+	_STD cout << "TimerSecondsDoEvent..." << '\n';
+}
+
+int main() {
+
+	Timer<_STD chrono::microseconds> timer_microseconds;
+	timer_microseconds.Start(1000000, TimerMicrosecondsDoEvent);
+
+	Timer<_STD chrono::milliseconds> timer_milliseconds;
+	timer_milliseconds.Start(1000, TimerMillisecondsDoEvent);
+
+	Timer<_STD chrono::seconds> timer_seconds;
+	timer_seconds.Start(1, TimerSecondsDoEvent);
+
+	for (;; _STD this_thread::sleep_for(_STD chrono::seconds(1)))
+		_STD cout << "Main Thread ..." << '\n';
+
+	return EXIT_SUCCESS;
+}
